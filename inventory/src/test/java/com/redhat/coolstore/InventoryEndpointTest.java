b0c7b681903f6d9b24bff9287afa83c83b0f6942
Daniel Oh <doh@redhat.com>
2023-10-14 22:36:58 -0400

    Add initial code

---
+++ inventory/src/test/java/com/redhat/coolstore/InventoryEndpointTest.java
@@ -0,0 +1,17 @@
+package com.redhat.coolstore;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+public class InventoryEndpointTest {
+
+    @Test
+    public void testListAllInventory() {
+        //List all, should have all 8 cities inventory the database has initially:
+       
+        //List a certain city(Seoul), 256 should be returned:
+       
+    }
+
+}
